d432b6d2aea495e83943e15f13d3d5626eca3de2
Mayank Gupta <114315030+Mayank22236@users.noreply.github.com>
2025-02-18 22:48:29 +0530

    push

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://movie-booking-backend-pxnb.onrender.com", // Add your backend URL
+    baseURL: "https://movie-booking-backend-pxnb.onrender.com/", // Add your backend URL
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${localStorage.getItem("token")}`,
